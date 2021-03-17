99fbe940db6280aeb14652707abe8868e8b9b620
Milen Gerov <milen.gerov@gmail.com>
2021-03-17 19:51:18 +0200

    logo-finder added

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -6,11 +6,16 @@ function Navigation() {
 
     return (
         <header className="header">
-            <h1><a class="home" href="#">About</a></h1>
+            <h1>
+                <a class="home" href="#">About</a>
+                <a href=""><img className="search-symbol "src="logo-finder-symbol.png" alt="" srcset="" width="40"/></a>
+                
+            </h1>
             <nav class="nav-buttons">
                 <a href="#">Create a wish</a>
-                <a href="#">Logout</a>
                 <a href="#">Register</a>
+                <a href="#">Login</a>
+                <a href="#">Logout</a>
             </nav>
         </header>
     );
